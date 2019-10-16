e2f092891356a5861ba5d7c7707124d9e27de813
Chiracolivier <chiracolivier28@gmail.com>
2019-10-16 23:15:33 +0200

    arranging PostListItem

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -2,7 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-post',
-  templateUrl: './post.component.html',
+  templateUrl: './PostListItem.component.html',
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
